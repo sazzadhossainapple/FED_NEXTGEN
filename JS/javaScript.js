0fc28124120851ffb5eac4a0a6a4e9b7770645ab
sazzadhossainapple <sazzadapple81@gmail.com>
2023-01-22 23:28:03 +0600

    img upload

--- JS/javaScript.js
+++ JS/javaScript.js
@@ -5,3 +5,16 @@ const navLinks = document.querySelectorAll(".hover-nav a").forEach((link) => {
     link.classList.add("active");
   }
 });
+
+// image upload
+const display = document.querySelector(".employee-profile-img");
+const input = document.querySelector("#imgUpload");
+let img = document.querySelector("img");
+
+input.addEventListener("change", () => {
+  let reader = new FileReader();
+  reader.readAsDataURL(input.files[0]);
+  reader.addEventListener("load", () => {
+    display.innerHTML = `<img src=${reader.result} alt=''/>`;
+  });
+});
